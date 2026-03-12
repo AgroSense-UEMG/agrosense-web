efd98ce32812fc08b7dafcdb02dc6cfffa0f5258
Cauã Cavalcante Gonçalves <gonssalvescaua@gmail.com>
2026-03-11 21:12:00 -0300

    add dashboard card de sensor

--- src/mocks/devices.ts
+++ src/mocks/devices.ts
@@ -69,16 +69,16 @@ export const mockDevices: InventoryDevice[] = [
 ];
 
 export const mockChartData = [
-  { time: "00:00", temperature: 22.5, humidity: 75 },
-  { time: "02:00", temperature: 21.0, humidity: 78 },
-  { time: "04:00", temperature: 19.5, humidity: 82 },
-  { time: "06:00", temperature: 18.2, humidity: 85 },
-  { time: "08:00", temperature: 20.5, humidity: 76 },
-  { time: "10:00", temperature: 24.0, humidity: 65 },
-  { time: "12:00", temperature: 27.5, humidity: 55 },
-  { time: "14:00", temperature: 29.8, humidity: 48 },
-  { time: "16:00", temperature: 28.5, humidity: 52 },
-  { time: "18:00", temperature: 25.0, humidity: 60 },
-  { time: "20:00", temperature: 23.5, humidity: 68 },
-  { time: "22:00", temperature: 22.8, humidity: 72 },
+  { time: "00:00", temperature: 22.5, humidity: 75, ph: 6.1, battery: 82 },
+  { time: "02:00", temperature: 21.0, humidity: 78, ph: 6.1, battery: 80 },
+  { time: "04:00", temperature: 19.5, humidity: 82, ph: 6.2, battery: 78 },
+  { time: "06:00", temperature: 18.2, humidity: 85, ph: 6.2, battery: 77 },
+  { time: "08:00", temperature: 20.5, humidity: 76, ph: 6.3, battery: 85 }, // Sol nasceu, bateria sobe
+  { time: "10:00", temperature: 24.0, humidity: 65, ph: 6.2, battery: 92 },
+  { time: "12:00", temperature: 27.5, humidity: 55, ph: 6.1, battery: 98 },
+  { time: "14:00", temperature: 29.8, humidity: 48, ph: 6.0, battery: 100 },
+  { time: "16:00", temperature: 28.5, humidity: 52, ph: 6.1, battery: 95 },
+  { time: "18:00", temperature: 25.0, humidity: 60, ph: 6.2, battery: 88 },
+  { time: "20:00", temperature: 23.5, humidity: 68, ph: 6.2, battery: 85 },
+  { time: "22:00", temperature: 22.8, humidity: 72, ph: 6.1, battery: 84 },
 ];
